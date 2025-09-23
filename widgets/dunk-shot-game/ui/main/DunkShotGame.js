219f842ff16df3a42e23845daa834b654340d779
johan <johan@email.ru>
2025-09-23 19:35:38 +0300

    feat(DunkShotWalls): добавлены стены к игре

--- widgets/dunk-shot-game/ui/main/DunkShotGame.js
+++ widgets/dunk-shot-game/ui/main/DunkShotGame.js
@@ -1,8 +1,14 @@
-import styles from "./DunkShotGame.module.scss";
 import {useLoadDunkShotScene} from "../../model/hooks/useLoadDunkShotScene";
+import DunkShotWalls from "../walls/DunkShotWalls";
+import styles from "./DunkShotGame.module.scss";
 
 export default function DunkShotGame() {
   const containerRef = useLoadDunkShotScene();
 
-  return <div ref={containerRef} className={styles.dunkShotContainer}/>;
+  return (
+    <>
+      <div ref={containerRef} className={styles.dunkShotContainer}/>
+      <DunkShotWalls containerRef={containerRef}/>
+    </>
+  );
 }
---
+++ widgets/dunk-shot-game/ui/walls/DunkShotWalls.js
@@ -0,0 +1,41 @@
+import {useEffect, useRef} from "react";
+import classNames from "classnames";
+import {LEFT, RIGHT} from "../../../../shared/constants/directions/directions";
+import {eventSubscription} from "../../../../shared/lib/events/eventListener";
+import gsap from "gsap";
+import {RESIZE} from "../../../../shared/constants/events/eventsNames";
+import styles from "./DunkShotWalls.module.scss";
+
+export default function DunkShotWalls({containerRef}) {
+  const wallRefs = useRef([]);
+
+  useEffect(() => {
+    const {current: walls} = wallRefs;
+    const {current: container} = containerRef;
+
+    const onResize = () => {
+      const {left, right, width} = container.getBoundingClientRect();
+
+      [left, right - width].forEach((width, index) => {
+        const currentWall = walls[index];
+        gsap.set(currentWall, {width});
+      });
+    };
+
+    onResize();
+
+    return eventSubscription({callbacksBus: [{event: RESIZE, callback: onResize}]});
+  }, []);
+
+  return (
+    <div className={styles.walls}>
+      {[LEFT, RIGHT].map((side, index) => (
+        <div
+          key={index}
+          className={classNames(styles.wall, styles[side])}
+          ref={ref => wallRefs.current[index] = ref}
+        />
+      ))}
+    </div>
+  );
+}
